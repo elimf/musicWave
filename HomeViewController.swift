726459c64a8ea0d2fce68a7611f36b84ba644613
Massimiliano <91194651+Massimiliano-HA@users.noreply.github.com>
2023-10-11 10:11:55 +0200

    initial storyboard

---
+++ HomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController.swift
+//  musicWave
+//
+//  Created by Massimiliano HUBERT-ABBATE on 11/10/2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+}
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        self.title = "Home"
+//        
+//        let item1 = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(self.actionBarButtonItem(_:)))
+//        
+//        let item2 = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: nil)
+//        
+//        self.navigationItem.rightBarButtonItems = [item1, item2]
+        
+        print("ViewDidLoad")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
