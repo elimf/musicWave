f5e2d4fbe04914f6f08f39df6b2ab1191610872d
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-10-12 10:40:40 +0200

    init view Artist

---
+++ musicWave/views/ArtistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ArtistViewController.swift
+//  musicWave
+//
+//  Created by Eddy on 12/10/2023.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
